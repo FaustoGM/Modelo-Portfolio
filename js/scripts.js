c8d587d153eed211c0589106f4e3be805d0a3e5e
Fausto <faustogiordanom@gmail.com>
2025-04-20 13:33:56 -0300

    Actualizo animación destacado, organizo CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
-const toggleTheme = document.getElementById('toggle-theme');
-const toggleIcon = document.getElementById('toggle-icon');
-const toggleText = document.getElementById('toggle-text');
-
-toggleTheme.addEventListener('click', ()=>{
-    document.body.classList.toggle('dark');
-    if(toggleIcon.src.includes('moon.svg')){
-        toggleIcon.src='assets/icons/sun.svg'
-        toggleText.textContent='Light Mode'
-    } else {
-        toggleIcon.src='assets/icons/moon.svg'
-        toggleText.textContent='Dark Mode'
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleTheme = document.getElementById('toggle-theme');
+    const toggleIcon = document.getElementById('toggle-icon');
+    const toggleText = document.getElementById('toggle-text');
+    const body = document.body;
+  
+    if (toggleTheme) {
+      toggleTheme.addEventListener('click', () => {
+        body.classList.toggle('dark');
+  
+        const isDark = body.classList.contains('dark');
+  
+        toggleIcon.src = isDark ? 'assets/icons/sun.svg' : 'assets/icons/moon.svg';
+        toggleText.textContent = isDark ? 'Light Mode' : 'Dark Mode';
+      });
     }
-
-})
+  });
+  
